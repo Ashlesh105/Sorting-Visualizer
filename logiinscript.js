647ce6a2a1fe9d12b7da1642a969040ce4ef9ffa
ashlesh <tashleshkini@gmail.com>
2023-08-18 14:57:41 +0530

    commit1

---
+++ logiinscript.js
@@ -0,0 +1,15 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+	event.preventDefault(); // prevent form submission
+  
+	// Get the input values
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+  
+	// Check if the username and password are correct
+	if (username === "Ashu" && password === "password" || username == "Swaru" && password == "password1") {
+	  document.getElementById("message").textContent = "Login successful!";
+	   window.location.href = "index.html";
+	} else {
+	  document.getElementById("message").textContent = "Invalid username or password!";
+	}
+  });
